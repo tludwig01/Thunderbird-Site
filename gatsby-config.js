8689068a047b46e45d106eee92151467f7a04d47
Joseph Shearer <joseph@josephshearer.net>
2020-03-25 20:46:51 -0400

    Lots of work on new gatsby potal that I didn't write a commit message for

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,14 @@
 module.exports = {
     plugins: [
-        "@jorsek/gatsby-theme-easydita-og"
-    ]
-}
+        {
+            resolve: "@jorsek/gatsby-theme-easydita-og",
+            options: {
+                CONTENT_RUNTIME_REMOTE: true,
+                CONTENT_BUILDTIME_REMOTE: true,
+                CONTENT_ORG: "acstech",
+                CONTENT_ROOTMAP: "9638ae30-e62b-11e9-90ab-0242437c4282",
+                CONTENT_TOKEN: "03b43cf46a9a4853be8b13e302e3d920",
+            },
+        },
+    ],
+};
