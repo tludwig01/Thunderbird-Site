b117bac4212e96cb71e036d334f3bdd12c7aed4d
Joseph Shearer <joseph@josephshearer.net>
2020-03-27 14:01:11 -0400

    Added a config example

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,29 @@ module.exports = {
         {
             resolve: "@jorsek/gatsby-theme-easydita-og",
             options: {
-                CONTENT_RUNTIME_REMOTE: true,
+                CONTENT_RUNTIME_REMOTE: false,
                 CONTENT_BUILDTIME_REMOTE: true,
-                CONTENT_ORG: "acstech",
-                CONTENT_ROOTMAP: "9638ae30-e62b-11e9-90ab-0242437c4282",
-                CONTENT_TOKEN: "03b43cf46a9a4853be8b13e302e3d920",
+                CONTENT_BUILDTIME_STATIC: false,
+                CONTENT_STATIC_DIR: "ezd_content",
+                CONTENT_ORG: "viewpoint",
+                CONTENT_ROOTMAP: "698054d0-703f-11ea-8573-0242c5886069",
+                CONTENT_TOKEN: "f3c5b7a944cf4197b706cf5c43fc5907",
+                CONTENT_TRANSFORMS: [
+                    {
+                        transformToPage(input) {
+                            return {
+                                ...input,
+                                breadcrumbs: [
+                                    {
+                                        href: input.breadcrumbs[2].href,
+                                        title: `${input.breadcrumbs[1].title} (${input.breadcrumbs[2].title})`,
+                                    },
+                                    ...input.breadcrumbs.slice(3),
+                                ],
+                            };
+                        },
+                    },
+                ],
             },
         },
     ],
