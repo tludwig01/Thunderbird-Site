606267ed7d89fa93616ac428536c18dbe95f6360
Joseph Shearer <joseph@josephshearer.net>
2020-04-01 20:26:46 -0400

    Configure correctly

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,12 +4,12 @@ module.exports = {
             resolve: "@jorsek/gatsby-theme-easydita-og",
             options: {
                 CONTENT_RUNTIME_REMOTE: false,
-                CONTENT_BUILDTIME_REMOTE: true,
-                CONTENT_BUILDTIME_STATIC: false,
+                CONTENT_BUILDTIME_REMOTE: false,
+                CONTENT_BUILDTIME_STATIC: true,
                 CONTENT_STATIC_DIR: "ezd_content",
-                CONTENT_ORG: "viewpoint",
-                CONTENT_ROOTMAP: "698054d0-703f-11ea-8573-0242c5886069",
-                CONTENT_TOKEN: "f3c5b7a944cf4197b706cf5c43fc5907",
+                CONTENT_ORG: "",
+                CONTENT_ROOTMAP: "",
+                CONTENT_TOKEN: "",
                 CONTENT_TRANSFORMS: [
                     {
                         transformToPage(input) {
