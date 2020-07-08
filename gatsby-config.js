efa4cd0d817015520f85c7e4b8666934ab1084b4
Joseph Shearer <joseph@josephshearer.net>
2020-07-07 20:07:03 -0400

    feature(gatsby-starter-easydita): Support `nconf` for file-based and environment-based config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,43 @@
-const portalConfig = require("./portal_config.json");
+/* eslint-disable @typescript-eslint/no-var-requires */
+const provider = require("nconf");
+
+provider
+    .file("./portal_config.json")
+    .env({
+        transform({ key, value }) {
+            if (!key.toLowerCase().startsWith("portal")) {
+                return false;
+            }
+            const k = key
+                .split("_")
+                .map((segment, idx) => (idx > 0 ? `${segment.toUpperCase()}${segment.slice(1)}` : segment))
+                .join("");
+
+            console.log(key, value);
+            return { key: k, value };
+        },
+        parseValues: true,
+        separator: ".",
+    })
+    .defaults({ contentClientOpts: {} });
 
 module.exports = {
     siteMetadata: {
-        heroTitle: portalConfig.heroTitle,
-        heroSubtitle: portalConfig.heroSubtitle,
+        heroTitle: provider.get("heroTitle"),
+        heroSubtitle: provider.get("heroSubtitle"),
     },
     plugins: [
         {
-            resolve: portalConfig.theme,
+            resolve: provider.get("theme"),
             options: {
-                CONTENT_RUNTIME_REMOTE: portalConfig.contentRuntimeRemote,
-                CONTENT_BUILDTIME_REMOTE: portalConfig.contentBuildtimeRemote,
-                CONTENT_BUILDTIME_STATIC: portalConfig.contentBuildtimeStatic,
-                CONTENT_STATIC_DIR: portalConfig.contentStaticDir,
-                CONTENT_ORG: portalConfig.contentOrg,
-                CONTENT_ROOTMAP: portalConfig.contentRootmap,
-                CONTENT_TOKEN: portalConfig.contentToken,
-                CONTENT_CLIENT_OPTS: portalConfig.contentClientOpts || {},
+                CONTENT_RUNTIME_REMOTE: provider.get("contentRuntimeRemote"),
+                CONTENT_BUILDTIME_REMOTE: provider.get("contentBuildtimeRemote"),
+                CONTENT_BUILDTIME_STATIC: provider.get("contentBuildtimeStatic"),
+                CONTENT_STATIC_DIR: provider.get("contentStaticDir"),
+                CONTENT_ORG: provider.get("contentOrg"),
+                CONTENT_ROOTMAP: provider.get("contentRootmap"),
+                CONTENT_TOKEN: provider.get("contentToken"),
+                CONTENT_CLIENT_OPTS: provider.get("contentClientOpts"),
             },
         },
     ],
